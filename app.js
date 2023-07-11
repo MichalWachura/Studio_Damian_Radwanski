dd127d4e8a62b93f25a8f12da0ee77e4c54a693a
Michał <130533795+MichalWachura@users.noreply.github.com>
2023-07-11 18:16:09 +0200

    Add files via upload
    Adding dark theme with saving settings in localStorage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 
-//  ============CURSOR============
+//  ============ CURSOR ============
 let innerCursor = document.querySelector(".inner-cursor");
 
 document.addEventListener('mousemove',moveCursor);
@@ -13,3 +13,62 @@ function moveCursor(e){
     innerCursor.style.left = `${x}px`;
     innerCursor.style.top = `${y}px`;
 }
+
+//  ============ DARK MODE ============
+
+let darkModeStorage = localStorage.getItem('darkMode');
+const darkThemeButton = document.querySelector(".day-night-background");
+
+
+
+
+const body = document.body;
+const navbar = document.querySelector('.navbar');
+const innerCursorDarkTheme = document.querySelector('.inner-cursor');
+const studentList = document.querySelector('.student-list');
+
+const footer = document.querySelector(".footer");
+
+
+const enableDarkMode = () =>{
+
+    body.classList.add("dark");
+    navbar.classList.add("dark");
+    innerCursorDarkTheme.classList.add("dark");
+    studentList.classList.add("dark");
+    studentContainer.classList.add("dark");
+    localStorage.setItem("darkMode","enabled");
+
+};
+
+
+const disableDarkMode = () =>{ 
+
+    body.classList.remove("dark");
+    navbar.classList.remove("dark");
+    innerCursorDarkTheme.classList.remove("dark");
+    studentList.classList.remove("dark");
+    studentContainer.classList.remove("dark");
+    localStorage.setItem("darkMode","disabled");
+
+};
+
+if (darkModeStorage === "enabled"){
+    enableDarkMode();
+}else{
+    
+    disableDarkMode();
+};
+
+darkThemeButton.addEventListener("click",()=>{
+    
+    darkModeStorage=localStorage.getItem('darkMode');
+
+    if(darkModeStorage !=="enabled"){
+    enableDarkMode();
+    console.log(darkModeStorage);
+    }else{
+        disableDarkMode();
+        console.log(darkModeStorage);
+    }
+});
