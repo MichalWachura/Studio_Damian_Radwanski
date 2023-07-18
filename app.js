f42b4a7f2a9778c36575a6ba62a2a7ad83eb5ffc
Michał <130533795+MichalWachura@users.noreply.github.com>
2023-07-18 08:20:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,75 @@ function moveCursor(e){
     innerCursor.style.top = `${y}px`;
 }
 
+//  ============ FILTER BY YEAR ============
+let allBtn = document.querySelector('#all-btn');
+
+let year2012Btn = document.getElementById("2012-btn");
+let year2019Btn = document.getElementById("2019-btn");
+let year2021Btn = document.getElementById("2021-btn");
+let year2022Btn = document.getElementById("2022-btn");
+
+let year2012 = document.querySelectorAll(".year2012");
+let year2019 = document.querySelectorAll(".year2019");
+let year2021 = document.querySelectorAll(".year2021");
+let year2022 = document.querySelectorAll(".year2022");
+
+
+
+allBtn.addEventListener("click",()=>{
+
+    year2012.forEach(year => year.classList.remove("hide"));
+    year2019.forEach(year => year.classList.remove("hide"));
+    year2021.forEach(year => year.classList.remove("hide"));
+    year2022.forEach(year => year.classList.remove("hide"));
+});
+
+year2012Btn.addEventListener("click",()=>{
+    year2012.forEach(year => year.classList.remove("hide"));
+    year2019.forEach(year => year.classList.add("hide"));
+    year2021.forEach(year => year.classList.add("hide"));
+    year2022.forEach(year => year.classList.add("hide"));
+});
+
+year2019Btn.addEventListener("click",()=>{
+    year2019.forEach(year => year.classList.remove("hide"));
+    year2012.forEach(year => year.classList.add("hide"));
+    year2021.forEach(year => year.classList.add("hide"));
+    year2022.forEach(year => year.classList.add("hide"));
+});
+
+year2021Btn.addEventListener("click",()=>{
+    year2021.forEach(year => year.classList.remove("hide"));
+    year2012.forEach(year => year.classList.add("hide"));
+    year2019.forEach(year => year.classList.add("hide"));
+    year2022.forEach(year => year.classList.add("hide"));
+});
+
+year2022Btn.addEventListener("click",()=>{
+    year2022.forEach(year => year.classList.remove("hide"));
+    year2012.forEach(year => year.classList.add("hide"));
+    year2019.forEach(year => year.classList.add("hide"));
+    year2021.forEach(year => year.classList.add("hide"));
+    
+});
+
+//  ============ SORTING NAMES ALPHABETICAL ============
+
+const alphaBtn = document.getElementById("alphabetic");
+let names = document.querySelectorAll(".name-surname");
+let namesValues = names.textContent;
+
+alphaBtn.addEventListener("click",()=>{
+    console.log("test");
+    let namesValues = [];
+    names.forEach((name) =>{
+        namesValues.push(name.textContent);
+    });
+    namesValues.sort()
+    console.log(namesValues);
+});
+
+
 //  ============ DARK MODE ============
 
 let darkModeStorage = localStorage.getItem('darkMode');
